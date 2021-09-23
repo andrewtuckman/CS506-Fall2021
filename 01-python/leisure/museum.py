1eb146cb0eb69e3157c98ccc2a3bb96b4933c7f3
andrewtuckman <tuckman@bu.edu>
2021-09-23 12:04:01 -0400

    Adding museum

--- 01-python/leisure/museum.py
+++ 01-python/leisure/museum.py
@@ -1,3 +1,25 @@
 def draw_museum():
-    print("museum not found")
+    
+    # Ceiling
+    print(33*"_")
+
+    for i in range(10):
+        if i == 4:
+            # Walls + Sign
+            print("|", 3*" ", "| El Museo del Prada |",2*" ", "|")
+        elif i==8 or i==9:
+            # Walls + Door frame
+            print("|", 12*" ", "| |",12*" ", "|")
+        elif i == 7:
+            # Walls + Top of Door
+            print("|", 12*" ", "___",12*" ", "|")
+        else:
+            # Rest of walls
+            print("|", 29*" ", "|")
+
+    # Floor
+    print(33*"-")
+
+
+
     return
